ee2e45b75a060a778244f0e9fef3fb1daecc0bc7
Marco <marcoleal21@outlook.com>
2023-02-20 17:33:19 -0800

    concatenated & moved variables to single statement

--- Basic JavaScript Projects/project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/project1_expressions_alert/JS/main.js
@@ -1,3 +1,5 @@
-window.alert("Hello, World!");
-var A = "Ha! You clicked the alert box!"
-document.write(A)
+var B = "Click OK," + " and get a special message!", A = "Ha! You clicked the alert box that said \"Click OK, and get a special message!\"";
+window.alert(B);
+document.write(A);
+document.write(" Thanks for checking out my small"
++ "website and I hope you liked it!");
