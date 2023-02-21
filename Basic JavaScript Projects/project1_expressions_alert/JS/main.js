27523951d67e86d26107b1923b1a566e9ddfc655
Marco <marcoleal21@outlook.com>
2023-02-21 15:58:14 -0800

    finished basic javascript projects assignment

--- Basic JavaScript Projects/project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/project1_expressions_alert/JS/main.js
@@ -1,5 +1,10 @@
-var B = "Click OK," + " and get a special message!", A = "Ha! You clicked the alert box that said \"Click OK, and get a special message!\"";
-window.alert(B);
-document.write(A);
-document.write(" Thanks for checking out my small"
-+ "website and I hope you liked it!");
+//ASSIGNING VARIABLES
+var alertBox = "Click OK," + " and get a special message!", sent1 = "This is the beginning of the string", sent2 = " and this is the end of the string ", expression1 = 6 + 1;
+
+
+//ADDING WINDOW ALERT
+window.alert(alertBox);
+
+
+//CONTENT
+document.write(sent1 + sent2 + expression1);
