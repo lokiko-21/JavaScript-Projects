918473f83742f4f16497715e222ecc45bf5d751c
Marco <marcoleal21@outlook.com>
2023-02-21 17:41:29 -0800

    finished subtraction function

--- Basic JavaScript Projects/project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/project3_math_operators/JS/main.js
@@ -1,5 +1,10 @@
 //FUNCTION
 function addFunction() {
     var add = 2 + 1;
-    document.getElementById("math").innerHTML = "2 + 1 = " + add;
+    document.getElementById("addition").innerHTML = "2 + 1 = " + add;
+}
+
+function subFunction() {
+    var sub = 8 - 4;
+    document.getElementById("subtraction").innerHTML = "8 - 4 = " + sub;
 }
