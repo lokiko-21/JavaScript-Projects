2e383dd119b19441941056fb9094b3482a7d2db2
Marco <marcoleal21@outlook.com>
2023-02-22 17:22:12 -0800

    used console.log

--- Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
@@ -1,7 +1,28 @@
-document.write(typeof 45 + " ");
-document.write("| 2" + 1);
-document.write(" | " + 2e310);
-document.write(" | " + -2e310)
+//JS CONTENT
+
+//USING TYPEOF
+document.write(typeof 45);
+document.write(" | ")
+
+//USING TYPE COERCION
+document.write("2" + 1);
+document.write(" | ")
+
+document.write(2e310);
+document.write(" | ")
+
+//USING (+/-)INFINITY
+document.write(-2e310);
+document.write(" | ")
+
+document.write(10 > 2);
+document.write(" | ")
+
+//BOOLEAN OPERATIONS
+document.write(10 < 2);
+
+
+//FUNCTIONS
 
 function myFunction() {
     document.getElementById("test1").innerHTML = 0/0;
@@ -13,3 +34,9 @@ function myFunction2() {
     document.getElementById("test4").innerHTML = 2e310;
     document.getElementById("test5").innerHTML = -2e310;
 }
+
+
+//CONSOLE
+
+console.log(6 + 9);
+console.log(9 > 10);
