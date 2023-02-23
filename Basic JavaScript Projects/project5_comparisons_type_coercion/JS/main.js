439599f23c3970f7cd6271177ff31fb064538141
Marco <marcoleal21@outlook.com>
2023-02-22 17:13:03 -0800

    used +/-infinity as well as isNaN

--- Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
@@ -1,2 +1,15 @@
-document.write(typeof 45);
-document.write(": 2" + 1);
+document.write(typeof 45 + " ");
+document.write("| 2" + 1);
+document.write(" | " + 2e310);
+document.write(" | " + -2e310)
+
+function myFunction() {
+    document.getElementById("test1").innerHTML = 0/0;
+    document.getElementById("test2").innerHTML = isNaN('21');
+    document.getElementById("test3").innerHTML = isNaN('hehe');
+}
+
+function myFunction2() {
+    document.getElementById("test4").innerHTML = 2e310;
+    document.getElementById("test5").innerHTML = -2e310;
+}
