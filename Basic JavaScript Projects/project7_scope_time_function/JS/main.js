246883a00fcd5f209869773e3ace15d376d1f0be
Marco <marcoleal21@outlook.com>
2023-02-26 11:07:32 -0800

    ran code with error and debugged using console.log

---
+++ Basic JavaScript Projects/project7_scope_time_function/JS/main.js
@@ -0,0 +1,11 @@
+function location1() {
+    var state = "WA";
+    document.write("I live in " + state + ". ");
+}
+
+var dream = "California"
+function location2() {
+    document.write(dream + " is where I want to live.")
+}
+location1();
+location2();
