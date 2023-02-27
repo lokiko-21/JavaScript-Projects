6c30a5cb874cc177711df08f2fe6237d3f9a841c
Marco <marcoleal21@outlook.com>
2023-02-26 16:57:42 -0800

    converted number to string

--- Basic JavaScript Projects/project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/project8_string_methods/JS/main.js
@@ -1,4 +1,4 @@
-function concatFunction() {
+function firstFunction() {
     var firstName = "Marco ";
     var lastName = "Breton";
     var middleName = "Antonio ";
@@ -9,4 +9,9 @@ function concatFunction() {
     document.getElementById("slice").innerHTML = section.toUpperCase() + " is a word that can be found withing my middle name.";
     document.getElementById("search").innerHTML = "Antonio starts in index position " +
     position + " within my full name: " + fullName;
+}
+
+function secondFunction() {
+    var myAge = 20;
+    document.getElementById("convertToString").innerHTML = myAge.toString();
 }
