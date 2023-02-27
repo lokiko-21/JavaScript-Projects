0646c6745ba132ed01ed9bcd41479317a4de628e
Marco <marcoleal21@outlook.com>
2023-02-26 16:47:51 -0800

    used slice method

--- Basic JavaScript Projects/project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/project8_string_methods/JS/main.js
@@ -3,5 +3,7 @@ function concatFunction() {
     var lastName = "Breton";
     var middleName = "Antonio ";
     var fullName = firstName.concat(middleName, lastName);
+    var section = middleName.slice(2, 5);
     document.getElementById("myName").innerHTML = "My name is " + fullName;
+    document.getElementById("slice").innerHTML = section + " is a word that can be found withing my middle name.";
 }
